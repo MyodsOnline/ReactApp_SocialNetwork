3bfe4be38685e4c1706109223a7f02904634384b
Andrew Smith <diver.vlz@gmail.com>
2022-04-23 14:11:23 +0300

    Fix bug

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainContent from './Components/Main/Main';
 import Dialogs from './Components/Dialogs/Dialogs';
 import Layout from './Components/Layout';
-<<<<<<< Updated upstream
-=======
 import Home from './Components/Home/Home';
 import Test from './Components/TestComponent/TestComponent';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />} >
+          <Route path="/" element={<Layout/>} >
+            <Route index element={<Home text="welcome!" />} />
             <Route path="posts" element={<MainContent />} />
             <Route path="dialogs" element={<Dialogs />} />
             <Route path="fake" element={<Test />} />
