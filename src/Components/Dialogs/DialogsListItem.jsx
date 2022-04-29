919133e021b634ee4bb9b611c13e5944208484b5
Andrew Smith <diver.vlz@gmail.com>
2022-04-30 00:47:04 +0300

    working with hardcoded props

--- src/Components/Dialogs/DialogsListItem.jsx
+++ src/Components/Dialogs/DialogsListItem.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import ds from "./Dialogs.module.css";
 
 import UserPhoto from "./../assets/img/user-3.jpeg";
 
-const DialogsListItem = () => {
+const DialogsListItem = (props) => {
+    let path = "./" + props.dLink;
     return (
-        <div className={ds.DialogsListItemStyle}>
-            <img src={UserPhoto} alt="user" />
-            <div className={ds.DialogData}>
-                <div className={ds.DialogDataHeader}>
-                    <h5>Contact Name</h5>
-                    <span>16</span>
-                </div>
-                <div className={ds.DialogDataText}>
-                    <p>DialogsListItem DialogsListItem DialogsListItem</p>
+        <NavLink to={path}>
+            <div className={ds.DialogsListItemStyle}>
+                <img src={UserPhoto} alt="user" />
+                <div className={ds.DialogData}>
+                    <div className={ds.DialogDataHeader}>
+                        <h5>{props.contactName}</h5>
+                        <span>16</span>
+                    </div>
+                    <div className={ds.DialogDataText}>
+                        <p>DialogsListItem DialogsListItem DialogsListItem</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </NavLink>
     )
 }
 
--- src/Components/Main/Posts/Posts.jsx
+++ src/Components/Main/Posts/Posts.jsx
@@ -3,8 +3,10 @@ import p from './Posts.module.css'
 import userImage from '../../assets/img/user-1.jpeg';
 import PostComment from './PostComment';
 
-const Posts = (proops) => {
-    const [like, setLike] = useState(Number(proops.likes))
+const Posts = (props) => {
+    const [like, setLike] = useState(Number(props.likes))
+
+    console.log(props);
 
     function increment () {
         setLike(like + 1)
@@ -15,11 +17,11 @@ const Posts = (proops) => {
             <img src={userImage} alt="user avatar"/>
             <div className={p.postBody}>
                 <div className={p.postBodyTitle}>
-                    <h4>Author Name</h4>
+                    <h4>{props.author}</h4>
                     <p>{like}<span onClick={increment}>♡</span></p>
                 </div>
                 <div className={p.postBodyText}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
+                    <p>{props.text}</p>
                     <PostComment message="Hi! Very funny!" />
                     <PostComment message="Dont be shine!" />
                 </div>
